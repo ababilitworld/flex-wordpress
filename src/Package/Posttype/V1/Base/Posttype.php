036adfb3755a3a02d405e2629209393d2f9da9a1
Ababil IT World <ababilitworld@gmail.com>
2025-06-17 19:12:16 +0600

    ababilithub/2025-06-17-19-12

--- src/Package/Posttype/V1/Base/Posttype.php
+++ src/Package/Posttype/V1/Base/Posttype.php
@@ -203,11 +203,11 @@ abstract class Posttype implements PosttypeContract
         }
     }
 
-    protected function use_block_editor_for_posttype($current_status, $post_type)
+    public function use_block_editor_for_posttype($current_status, $post_type)
     {
         // Debug output (check your error log)
     error_log("Checking block editor for {$post_type}, should be {$this->use_block_editor}");
-    
+
         if ($post_type === $this->posttype) 
         {
             return $this->use_block_editor;
