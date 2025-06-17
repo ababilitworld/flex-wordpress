b794776aaf0f571a60e95585de36525bd8cbcb31
Ababil IT World <ababilitworld@gmail.com>
2025-06-17 22:35:59 +0600

    ababilithub/2025-06-17-22-35

--- src/Package/Posttype/V1/Contract/Posttype.php
+++ src/Package/Posttype/V1/Contract/Posttype.php
@@ -6,5 +6,5 @@ interface Posttype
     public function get_slug(): string;
     public function register_post_type(): void;
     public function add_taxonomy(string $taxonomy_slug): void;
-    public function add_meta(array $meta): void; 
+    public function add_post_meta(int $post_id, array $meta_data, bool $update_existing = true): array; 
 }
