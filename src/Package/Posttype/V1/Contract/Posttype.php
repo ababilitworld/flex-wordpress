6c6f52998084e013001fdb2216288ef166897b45
Ababil IT World <ababilitworld@gmail.com>
2025-06-21 13:09:30 +0600

    ababilithub/2025-06-21-13-09

--- src/Package/Posttype/V1/Contract/Posttype.php
+++ src/Package/Posttype/V1/Contract/Posttype.php
@@ -3,7 +3,6 @@ namespace Ababilithub\FlexWordpress\Package\Posttype\V1\Contract;
 
 interface Posttype
 {
-    public function get_slug(): string;
     public function register_post_type(): void;
      
 }
