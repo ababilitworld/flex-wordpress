8a311cfdd07d694afcde6bd8b72efc819b8584c2
Ababil IT World <ababilitworld@gmail.com>
2025-08-09 22:03:06 +0600

    ababilitworld/2025-08-09-22-02

--- src/Package/OptionBox/V1/Concrete/VerticalTabBox/OptionBox.php
+++ src/Package/OptionBox/V1/Concrete/VerticalTabBox/OptionBox.php
@@ -26,20 +26,26 @@ class OptionBox extends BaseOptionBox
         <div class="fpba">
             <div class="meta-box">
                 <div class="app-container">
-                    <div class="vertical-tabs">
-                        <div class="tabs-header">
-                            <button class="toggle-tabs" id="toggleTabs">
-                                <i class="fas fa-chevron-left"></i>
-                            </button>
-                            <span class="tabs-title"><?php echo $this->title;?></span>
+                    <form method="post" action="">
+                        <?php wp_nonce_field($this->id.'_nonce_action'); ?>
+                        <input type="hidden" name="option_page" value="<?php echo esc_attr($this->id); ?>">
+                        
+                        <div class="vertical-tabs">
+                            <div class="tabs-header">
+                                <button class="toggle-tabs" id="toggleTabs">
+                                    <i class="fas fa-chevron-left"></i>
+                                </button>
+                                <span class="tabs-title"><?php echo $this->title;?></span>
+                            </div>
+                            <ul class="tab-items">
+                                <?php do_action($this->id.'_'.'tab_item'); ?>
+                            </ul>
                         </div>
-                        <ul class="tab-items">
-                            <?php do_action($this->id.'_'.'tab_item'); ?>
-                        </ul>
-                    </div>
-                    <main class="content-area">
-                        <?php do_action($this->id.'_'.'tab_content'); ?>
-                    </main>
+                        <main class="content-area">
+                            <?php do_action($this->id.'_'.'tab_content'); ?>
+                        </main>
+                        <?php submit_button(__('Save Settings', 'text-domain')); ?>
+                    </form>
                 </div>
             </div>
         </div>
