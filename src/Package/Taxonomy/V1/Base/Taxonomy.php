e6ccb167c03598e146d94f39ae9b95cbf2d2f1a1
Ababil IT World <ababilitworld@gmail.com>
2025-09-02 04:04:02 +0600

    ababilitworld/2025-09-02-04-04

--- src/Package/Taxonomy/V1/Base/Taxonomy.php
+++ src/Package/Taxonomy/V1/Base/Taxonomy.php
@@ -129,31 +129,6 @@ abstract class Taxonomy implements TaxonomyContract
     }
 
     /**
-     * Add "View Metas" action link to term list rows
-     */
-    public function add_action_view_details(array $actions, \WP_Term $term): array
-    {
-        // Check if this is our taxonomy
-        if ($term->taxonomy !== $this->taxonomy) 
-        {
-            return $actions;
-        }
-
-        // Always show the link (remove meta check for now)
-        $actions['view_details'] = sprintf(
-            '<a href="%s" aria-label="%s">%s</a>',
-            esc_url(admin_url(sprintf(
-                'admin.php?page=flex-supervisor-audit-term&object_id=%d&action_id=view_details',
-                $term->term_id
-            ))),
-            esc_attr(sprintf(__('View meta for "%s"', 'flex-eland'), $term->name)),
-            esc_html__('View Details', 'flex-eland')
-        );
-
-        return $actions;
-    }
-    
-    /**
      * Add image field to add new term form
      */
     public function add_term_image_field($taxonomy): void
