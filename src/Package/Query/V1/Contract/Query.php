4444cbe193c5e2842ae402990cbba9e0e6502081
Ababil IT World <ababilitworld@gmail.com>
2025-10-02 07:48:18 +0600

    ababilitworld/2025-10-02-07-48

--- src/Package/Query/V1/Contract/Query.php
+++ src/Package/Query/V1/Contract/Query.php
@@ -5,7 +5,7 @@ namespace Ababilithub\FlexWordpress\Package\Query\V1\Contract;
 
 interface Query 
 {
-    public function init(array $data = []): static;
+    //public function init(array $data = []): static;
     public function set_custom_args(array $args, string $option = 'merge'): static;
     public function get_args(): array;
     public function reset(): static;
