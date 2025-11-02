7877f4e5fa23f2622ae64aa800511a38a49edf3f
Ababil IT World <ababilitworld@gmail.com>
2025-11-03 01:05:16 +0600

    ababilitworld/2025-11-03-01-05

--- src/Package/Postmeta/V1/Contract/Postmeta.php
+++ src/Package/Postmeta/V1/Contract/Postmeta.php
@@ -2,7 +2,7 @@
 namespace Ababilithub\FlexWordpress\Package\PostMeta\V1\Contract;
 
 interface PostMeta
-{
+{ 
     public function init(): void;
     public function register(): void;
     public function save($post_id);
