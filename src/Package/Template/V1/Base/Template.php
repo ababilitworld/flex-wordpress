fb399eb57c116adf005cafe6dd550e58c2305dd4
Ababil IT World <ababilitworld@gmail.com>
2026-07-25 05:49:38 +0600

    ababilithub/2026-07-25-05-49

--- src/Package/Template/V1/Base/Template.php
+++ src/Package/Template/V1/Base/Template.php
@@ -6,16 +6,53 @@ use Ababilithub\{
 };
 
 abstract class Template implements TemplateContract
-{   
-    protected $asset_base_url;
-    protected $asset_base_prefix;
+{
+    protected array $config = [];
+
+    protected array $default_config = [];
+
+    protected string $asset_base_url = '';
+
+    protected string $asset_base_prefix = '';
 
     public function __construct(array $data = [])
     {
         $this->init($data);
     }
 
-    abstract public function init(array $data = []) : static;
-    abstract public function render() : bool|string;
+    abstract public function init(array $data = []): static;
 
+    abstract public function render(array $data = []): string;
+
+    public function set_config(array $config = []): static
+    {
+        $this->config = array_replace_recursive(
+            $this->default_config,
+            $config
+        );
+
+        return $this;
+    }
+
+    public function get_config(): array
+    {
+        return $this->config;
+    }
+
+    public function set_default_config(array $default_config = []): static
+    {
+        $this->default_config = $default_config;
+
+        return $this;
+    }
+
+    public function get_default_config(): array
+    {
+        return $this->default_config;
+    }
+
+    protected function get_config_value( string $key, mixed $default = null ): mixed 
+    {
+        return $this->config[$key] ?? $default;
+    }
 }
